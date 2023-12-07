a17cb4eee13a2b31783c92c9d3becc37ffd3afc6
AhmadAlkhateeb <151034379+ahmad-al-khatee-b@users.noreply.github.com>
2023-12-07 08:52:21 -0800

    Questions

---
+++ Questions/app.js
@@ -0,0 +1,9 @@
+const text = document.querySelectorAll(".question");
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn){
+ btn.addEventListener('click' , function(e){
+    const question = e.currentTarget.parentElement.parentElement
+    question.classList.toggle('show-text');
+ });
+});
